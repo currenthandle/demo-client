624375ab09c21c2e71ce15f8080f7b5c7f81a12d
Casey <leptone55@gmail.com>
2023-01-08 20:25:56 -0800

    remove unneeded

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import { type NextPage } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
 import Configuration from '../components/Configuration';
 import { z } from 'zod';
